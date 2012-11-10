3972380a2965563a06215802626130610be4b29c
Ramiro Gomez <web@ramiro.org>
2012-11-10 01:04:28 +0100

    check indicators

--- maketestcsv.sh
+++ maketestcsv.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-head -n1 WDI_GDF_Data.csv > test.csv
-grep "$1" WDI_GDF_Data.csv >> test.csv
+# call with indicator ID like EN.POP.DNST   
+head -n1 WDI_GDF_Data.csv | cut -d "," -f 1,45-55 > $1.csv
+
+# use perl for problematic country names like "Bahamas, The" or "Egypt, Arab Rep."
+grep "$1" WDI_GDF_Data.csv | perl -pe 's/^"([^,]+)[^"]+"/\1/' | cut -d "," -f 1,45-55 >> $1.csv
